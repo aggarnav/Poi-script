e84aa34d2412173e90b5af2bbb0fc005f3b07d4d
arnav aggarwal <aggarnav@gmail.com>
2018-12-14 12:08:12 +0530

    First Commit

---
+++ POI.py
@@ -0,0 +1,37 @@
+import requests, json, sys
+
+# enter your api key here
+
+# url variable store url
+url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
+
+# The file to create
+f = open("pois.xml", "a")
+
+
+# get method of requests module
+# return response object
+key = str(input("enter places api key: "))
+while True:
+    query = input("enter the query: ")
+    if query == 'stop':
+        f.close()
+        sys.exit()
+    r = requests.get(url + 'query=' + query +
+                     '&key=' + key)
+
+    # json method of response object convert
+    #  json format data into python format data
+    x = r.json()
+    y = x["results"]
+
+    for i in range(len(y)):
+        print(str(i) + ": " + y[i]['name'])
+    index = input('The index of place you want to add: ')
+    name = y[int(index)]['name']
+    lat = y[int(index)]['geometry']['location']['lat']
+    lng = y[int(index)]['geometry']['location']['lng']
+    heading = input("Enter required heading: ")
+    tilt = input("Enter required tilt: ")
+    f.write("Earth@"+name+"@flytoview=<LookAt><longitude>"+str(lng)+"</longitude><latitude>"+str(lat)+"</latitude><altitude>0</altitude><heading>"+heading+"</heading><tilt>"+tilt+"</tilt><gx:altitudeMode>relativeToSeaFloor</gx:altitudeMode></LookAt>")
+f.close()
---
+++ README.md
@@ -0,0 +1,1 @@
+The code is very simple to run and prompts are displayed to guide the user. In case you want to exit the script then type stop when it asks you for a query and the script would stop execution and save the xml file :)
