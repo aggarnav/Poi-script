fb57700af1452567c351365f069617f255c3c24b
aggarnav <aggarnav@gmail.com>
2018-12-14 23:28:20 +0530

    Update POI.py

--- POI.py
+++ POI.py
@@ -1,4 +1,4 @@
-import requests, json, sys
+import requests, json, sys, time
 
 # enter your api key here
 
@@ -12,26 +12,33 @@ f = open("pois.xml", "a")
 # get method of requests module
 # return response object
 key = str(input("enter places api key: "))
-while True:
-    query = input("enter the query: ")
-    if query == 'stop':
-        f.close()
-        sys.exit()
-    r = requests.get(url + 'query=' + query +
-                     '&key=' + key)
 
-    # json method of response object convert
-    #  json format data into python format data
-    x = r.json()
-    y = x["results"]
 
-    for i in range(len(y)):
-        print(str(i) + ": " + y[i]['name'])
-    index = input('The index of place you want to add: ')
-    name = y[int(index)]['name']
-    lat = y[int(index)]['geometry']['location']['lat']
-    lng = y[int(index)]['geometry']['location']['lng']
-    heading = input("Enter required heading: ")
-    tilt = input("Enter required tilt: ")
-    f.write("\nEarth@"+name+"@flytoview=<LookAt><longitude>"+str(lng)+"</longitude><latitude>"+str(lat)+"</latitude><altitude>0</altitude><heading>"+heading+"</heading><tilt>"+tilt+"</tilt><gx:altitudeMode>relativeToSeaFloor</gx:altitudeMode></LookAt>")
-f.close()
+def poi():
+    while True:
+        query = input("enter the query: ")
+        if query.lower() == 'stop'.lower():
+            f.close()
+            sys.exit()
+        r = requests.get(url + 'query=' + query +
+                         '&key=' + key)
+
+        # json method of response object convert
+        #  json format data into python format data
+        x = r.json()
+        if str(x["status"]) == "OVER_QUERY_LIMIT":
+            print("exceeded per second limit, stopping for some time, change the query just to reload the get request once input is asked for")
+            time.sleep(20)
+            poi()
+        y = x["results"]
+
+        for i in range(len(y)):
+            print(str(i) + ": " + y[i]['name'])
+        index = input('The index of place you want to add: ')
+        name = y[int(index)]['name']
+        lat = y[int(index)]['geometry']['location']['lat']
+        lng = y[int(index)]['geometry']['location']['lng']
+        heading = input("Enter required heading: ")
+        tilt = input("Enter required tilt: ")
+        f.write("\nEarth@"+name+"@flytoview=<LookAt><longitude>"+str(lng)+"</longitude><latitude>"+str(lat)+"</latitude><altitude>0</altitude><heading>"+heading+"</heading><tilt>"+tilt+"</tilt><gx:altitudeMode>relativeToSeaFloor</gx:altitudeMode></LookAt>")
+poi()
